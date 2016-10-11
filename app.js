8f7f66087bc0d12b9515900f22cdc2e8d08e2d8d
rinkumalik11 <rinkumalik11@gmail.com>
2016-10-10 22:39:15 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+(function (){
+
+	'use strict';
+
+			var app = angular.module('app', []).
+		controller('MainController', myFunction);
+
+		myFunction.$inject = ['$scope'];
+
+		function myFunction ($scope){
+
+			$scope.message = " ";
+
+			$scope.prof_name = "YAAKOV CHAIKIN";
+
+			$scope.stu_name = "RINKU MALIK";
+
+			$scope.eatables = "";
+
+			$scope.value = 0;
+
+			$scope.cal = function (){
+
+				var totalItems = stringSplit($scope.eatables);
+
+				$scope.value = totalItems;
+
+				if(totalItems == 0){
+					$scope.message = "Please enter something";
+				}else if(totalItems <= 3){
+					$scope.message = "Enjoy!";
+				}else{
+					$scope.message = "TOO MUCH!";
+				}
+			};
+
+			function stringSplit(string){
+				var array = string.split(",");
+
+				var x = array.length;
+
+				return x;
+			};
+
+
+
+		};
+
+})();
